aca69b86457face3e1fd1b77086d6096be6246da
Aluízio <aluizio2210@gmail.com>
2021-03-02 02:17:32 -0300

    Criado a pasta controllers e arquivo UserController

---
+++ controllers/UserController.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../models");
+
+module.exports = {
+
+  async authenticate(req, res, next) {
+
+    let { email, password } = req.body;
+    let user = await User.findOne({ where: { email } });
+    if(!user){
+      return res.render('cp', { notFound: true });
+    }
+
+    if(!bcrypt.compareSync(password, user.password)){
+      return res.render('cp', { notFound: true });
+    }
+
+    // removendo a propriedade password para nao criar sessao
+    // contendo a senha do usuario logado
+    delete user.password;
+
+    req.session.user = user;
+
+    res.render('cp', { user: req.session.user });
+  },
+}
